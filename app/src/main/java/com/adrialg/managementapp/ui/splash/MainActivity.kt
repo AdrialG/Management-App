336f7fd82cc84d7ea043c5225f03a3a9227c1de5
AdrialG <omahguntur@gmail.com>
2023-06-20 16:52:56 +0700

    First Commit

---
+++ app/src/main/java/com/adrialg/managementapp/ui/splash/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.adrialg.managementapp.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adrialg.managementapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/adrialg/managementapp/ui/verification/UserVerificationActivity.kt
@@ -0,0 +1,11 @@
+package com.adrialg.managementapp.ui.verification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserVerificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
